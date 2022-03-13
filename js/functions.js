fdd52219ea52e4e4e1a82c3041eaf6308d3f0d54
sbs0223 <100311853+sbs0223@users.noreply.github.com>
2022-03-13 13:51:56 -0400

    merge js

--- js/functions.js
+++ js/functions.js
@@ -3,30 +3,22 @@ needs to be manually sorted by date & clipped to 4 entries atm with the json raw
 
 	function getlatest() {
 
-		fetch("/json/latest")
-		.then(response => response.json())
-		.then((latestdata) => {
-			var latest = "";
-			updates = latestdata.updates;
-			
-			for (var entry in updates) {
-				title = updates[entry].title;
-				url = updates[entry].url;
-				thumb = updates[entry].thumb;
-				chapter = updates[entry].chapter;
-				d = new Date(updates[entry].date*1000);
-				updatedate = d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate();
-			
-				latest += "<div class=\"latestentrywrap\">";
-				latest += "<a href=\"" + url + "\">";
-				latest += "<img src=\"" + thumb + "\" class=\"latestimg\"></a><span class=\"chapter\">";
-				latest += title + " Ch. " + chapter + "</span><br>";
-				latest += "<span class=\"date\">" + updatedate + "<span></div>";
+		alasql.promise("SELECT top(4) series, projectname, projectsmallthumb, max(timestamp) as time_stamp FROM json('/json/chapters') group by series, projectname, projectsmallthumb order by max(timestamp) desc, series desc"
+		).then(function(results){
+			var latest = ""
+			for(var i = 0; i < results.length; i++) {
+			    var obj = results[i];
+					var d = new Date(obj.time_stamp*1000);
+					var chtime = d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate();
 
+					latest += "<div class=\"latestentrywrap\">";
+					latest += "<a href=\"projects?n=" + obj.projectname + "\">";
+					latest += "<img src=\"" + obj.projectsmallthumb + "\" class=\"latestimg\"></a><span class=\"chapter\">";
+					latest += obj.series + "</span><br>";
+					latest += "<span class=\"date\">" + chtime + "<span></div>";
 			}
-	
+
 			document.getElementById("latestcontainer").innerHTML = latest;
-			
 		});	
 	}
 
@@ -57,37 +49,33 @@ needs to be manually sorted by date & clipped to 4 entries atm with the json raw
 	
 	
 	
-	
+
 // Start Get URL function for the reader
 	
 	function geturl() {
-		fetch("/json/mapping.json")
-		.then(response => response.json())
-		.then((mappingdata) => {
-			var links = mappingdata.projects;
+	alasql.promise("SELECT distinct series, projectname, projectrating, projecturl FROM json('/json/chapters') where projectname = '" + series + "'"
+	).then(function(results){
+		var embedurl = "";
+		proj = results[0];
 			
 			try {
-				embedurl = links[series].url;
+				embedurl = proj.projecturl;
 			}
 			catch(err) {				
 				window.location.replace("/404");   // send wrong series names to 404
 			}
 			
-			adult = links[series].adult;
-			
 			if ( number === "" || number === "null" ) {
-				embedurl = links[series].url;
+				embedurl = proj.projecturl;
 			} else {
-				embedurl = links[series].url + number;
+				embedurl = proj.projecturl + number;
 			};
 						
-			if (!embedurl) {
-				window.location.replace("/404");  //send wrong chapter numbers to 404
-			} else if ( adult === "Y" ) {
+			if ( proj.projectrating === "Y" ) {
 				showUserAlerts();   // trigger swal if 18+ series
-			} else { };  // no additional action if non-mature series with valid chapter
+			} else { };  // no additional action if non-mature series
 			
-			var projectpagelink = "<a href=\"/projects?n=" + series + "\"> Project Page &gt;</a>";
+			var projectpagelink = "<a href=\"/projects?n=" + proj.projectname + "\"> Project Page &gt;</a>";
 			
 			embed = "<embed type=\"text/html\" src=\"" + embedurl + "\" width=\"100%\" height=\"100%\">";     
 			document.getElementById("reader").innerHTML = embed;
@@ -100,113 +88,112 @@ needs to be manually sorted by date & clipped to 4 entries atm with the json raw
 	
 	
 	
-	
-	
-	
 // Start Get index function for project page
 	
 	function getindex() {
-		fetch("/json/mapping.json")
-		.then(response => response.json())
-		.then((mappingdata) => {
+
+		alasql.promise("SELECT distinct series, projectname, projectrating, projectthumb, projectstatus FROM json('/json/chapters') order by series asc"
+		).then(function(results){
+			var latest = ""
 			var seriesindex = "";
 			var changedheader = "";
-			var projects = mappingdata.projects;
-			
-			for (var entry in projects) {
-			
-				if (projects[entry].adult == "Y") {
-					var adult = "18+";
-				} else { var adult = "";};
-				var fullname = projects[entry].fullname;
-				var thumb = projects[entry].thumb;
-				var statustype = projects[entry].status;
+			for(var i = 0; i < results.length; i++) {
+			    var obj = results[i];
+
+					if (obj.projectrating == "Y") {
+						var adult = "18+";
+					} else { var adult = "";};
 				
-				if (type == "" || type == "null" || type == "all") {
-					seriesindex += "<div class=\"projectwrap\">";
-					seriesindex += "<a href=\"?n=" + entry + "\">";
-					seriesindex += "<img src=\"" + thumb + "\" class=\"projectthumb\">";
-					seriesindex += "<h1>" + "<span>" + adult + "</span>" + fullname + "</h1></a>";
-					seriesindex += "</div>";
-					changedheader = "All Projects";
-				} else {
-					if ( statustype == type ) {
+					if (type == "" || type == "null") {
+						if ( obj.projectstatus == "current" || obj.projectstatus == "complete" ) {  // show current & complete if default projects page
+							seriesindex += "<div class=\"projectwrap\">";
+							seriesindex += "<a href=\"?n=" + obj.projectname + "\">";
+							seriesindex += "<img src=\"" + obj.projectthumb + "\" class=\"projectthumb\">";
+							seriesindex += "<h1>" + "<span>" + adult + "</span>" + obj.series + "</h1></a>";
+							seriesindex += "</div>";
+							changedheader = "Current & Complete";
+						}
+					} else if (!(type == "all") && !(type == "current") && !(type == "dropped") && !(type == "licensed") && !(type == "complete")) {window.location.replace("/404");}; // send to 404 if not one of the valid statuses
+				
+					if (type == "all") {
 						seriesindex += "<div class=\"projectwrap\">";
-						seriesindex += "<a href=\"?n=" + entry + "\">";
-						seriesindex += "<img src=\"" + thumb + "\" class=\"projectthumb\">";
-						seriesindex += "<h1>" + "<span>" + adult + "</span>" + fullname + "</h1></a>";
+						seriesindex += "<a href=\"?n=" + obj.projectname + "\">";
+						seriesindex += "<img src=\"" + obj.projectthumb + "\" class=\"projectthumb\">";
+						seriesindex += "<h1>" + "<span>" + adult + "</span>" + obj.series + "</h1></a>";
 						seriesindex += "</div>";
-						changedheader = type;
+						changedheader = "All Projects";
+					} else {
+						if ( obj.projectstatus == type ) {
+							seriesindex += "<div class=\"projectwrap\">";
+							seriesindex += "<a href=\"?n=" + obj.projectname + "\">";
+							seriesindex += "<img src=\"" + obj.projectthumb + "\" class=\"projectthumb\">";
+							seriesindex += "<h1>" + "<span>" + adult + "</span>" + obj.series + "</h1></a>";
+							seriesindex += "</div>";
+							changedheader = type;
+						}
 					}
-				}
-				
-			};			
-			
+			}
 			document.getElementById("seriescontainer").innerHTML = seriesindex;
 			document.getElementById("projectheader").innerHTML = changedheader;
-			
-			});
-		};
+		});	
+	}
 	
-	// Start Get chapters function for individual project page
-	
-		function getchapters() {
-			fetch("/json/mapping.json")
-			.then(response => response.json())
-			.then((mappingdata) => {
-				var projectdetail = "";
-				var projects = mappingdata.projects;	
-				
-				var url = projects[series].url;
-				if (projects[series].adult == "Y") {
-					var adult = "<img src=\"/images/assets/adult.png\" class=\"smallicons\">";
-				} else { var adult = "<img src=\"/images/assets/15r.png\" class=\"smallicons\">";};
-				var fullname = projects[series].fullname;
-				var descrjs = projects[series].description;
-				var description = descrjs.replace(/\n/g,"<br>");
-				var mulink = projects[series].mulink;
-				var thumb = projects[series].thumb;
-				var chapnum = projects[series].num;
-				var type = projects[series].status;
-				
-				projectdetail += "<div class=\"projecttopwrap\" align=center>";
-				projectdetail += "<div class=\"projectpagecover\"><img src=\"" + thumb + "\"></div>";
-				projectdetail += "<div class=\"projectpageinfo\"><div id=\"container\">";
-				projectdetail += "<span>" + adult + "</i> <a href=\"" + mulink + "\"><img src=\"/images/assets/muicon.svg\" class=\"smallicons\"></a> ";
-				if (type == "licensed") {
-					projectdetail += "<a href=\"" + url + "\"><img src=\"/images/assets/licensed.png\" class=\"smallicons\"></a>";
-				} else {
-					projectdetail += "<a href=\"" + url + "\"><img src=\"/images/assets/cubari.png\" class=\"smallicons\"></a> <a href=\"/read?series=" + series + "\"><img src=\"/images/android-chrome-192x192.png\" class=\"smallicons\"></a>";
-				}
-				projectdetail += "</span>";
-				projectdetail += description + "<button class=\"read-more\">more/less</button>";
-				projectdetail += "</div></div></div><div class=\"subcontentcontainer\"><h2>Chapters</h2>";
-				projectdetail += "<div class=\"projectouterwrap\" align=center><div class=\"parentproject\">";
-				
-				for (var entry in chapnum) {
-					var d = new Date(chapnum[entry].timestamp*1000);
-					var chtime = d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate();
-					var chtitle = chapnum[entry].chtitle;
-					var previewimg = chapnum[entry].previewimg;
-					
-					projectdetail += "<div class=\"projectwrap\">";
-					projectdetail += "<a href=\"/read?series=" + series + "&num=" + entry + "\">";
-					projectdetail += "<img src=\"" + previewimg + "\" class=\"projectthumb\">";
-					projectdetail += "<h1>" + "<span>" + chtime + "</span>" + chtitle + "</h1></a>";
-					projectdetail += "</div>";
-					
-				};
-				projectdetail += "</div></div>";
-				
-				document.getElementById("projectheader").innerHTML = fullname;
-				document.getElementById("seriescontainer").innerHTML = projectdetail;
-				document.getElementById("projectsnavwrapper").innerHTML = ""
-				
-				$( document ).ready(function() {
-				    $('.read-more').click(function(){
-				        $(this).parent().toggleClass('expanded');
-				    });
-				});
 
-				});
-			}	
+
+
+// Start Get chapters function for individual project page
+
+	function getchapters() {
+		alasql.promise("SELECT series, projectname, projectdesc, projectmu, projecturl, projectrating, projectthumb, projectstatus, timestamp, num, chname, chthumb FROM json('/json/chapters') where projectname = '" + series + "' order by num desc"
+		).then(function(results){
+			var projectdetail = "";
+			var proj = results[0];
+			
+			if (proj.projectrating == "Y") {
+				var adult = "<img src=\"/images/assets/adult.png\" class=\"smallicons\">";
+			} else { var adult = "<img src=\"/images/assets/15r.png\" class=\"smallicons\">";};
+			var descrjs = proj.projectdesc;
+			var description = descrjs.replace(/\n/g,"<br>");
+			
+			projectdetail += "<div class=\"projecttopwrap\" align=center>";
+			projectdetail += "<div class=\"projectpagecover\"><img src=\"" + proj.projectthumb + "\"></div>";
+			projectdetail += "<div class=\"projectpageinfo\"><div id=\"container\">";
+			projectdetail += "<span>" + adult + "</i> <a href=\"" + proj.projectmu + "\"><img src=\"/images/assets/muicon.svg\" class=\"smallicons\"></a> ";
+			if (proj.projectstatus == "licensed") {
+				projectdetail += "<a href=\"" + proj.projecturl + "\"><img src=\"/images/assets/licensed.png\" class=\"smallicons\"></a>";
+			} else {
+				projectdetail += "<a href=\"" + proj.projecturl + "\"><img src=\"/images/assets/cubari.png\" class=\"smallicons\"></a> <a href=\"/read?series=" + proj.projectname + "\"><img src=\"/images/android-chrome-192x192.png\" class=\"smallicons\"></a>";
+			}
+			projectdetail += "</span>";
+			projectdetail += description + "<button class=\"read-more\">more/less</button>";
+			projectdetail += "</div></div></div><div class=\"subcontentcontainer\"><h2>Chapters</h2>";
+			projectdetail += "<div class=\"projectouterwrap\" align=center><div class=\"parentproject\">";
+			
+			if (!(proj.projectstatus == "licensed")) {
+				for(var i = 0; i < results.length; i++) {
+					var obj = results[i];
+					var d = new Date(obj.timestamp*1000);
+					var chtime = d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate();
+			
+					projectdetail += "<div class=\"projectwrap\">";
+					projectdetail += "<a href=\"/read?series=" + obj.projectname + "&num=" + obj.num + "\">";
+					projectdetail += "<img src=\"" + obj.chthumb + "\" class=\"projectthumb\">";
+					projectdetail += "<h1>" + "<span>" + chtime + "</span>" + obj.chname + "</h1></a>";
+					projectdetail += "</div>";
+				}
+			} else {
+					projectdetail += "<center>Licensed. Please support the author by <a href=\"" + proj.projecturl + "\" target=\"_blank\">purchasing the official releases</a>!<br><br></center>";
+			};
+			projectdetail += "</div></div>"; 
+			
+			document.getElementById("projectheader").innerHTML = proj.series;
+			document.getElementById("seriescontainer").innerHTML = projectdetail;
+			
+			$( document ).ready(function() {
+			    $('.read-more').click(function(){
+			        $(this).parent().toggleClass('expanded');
+			    });
+			});
+
+			});
+		}	
