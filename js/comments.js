f6db1ac610c0c001cc2131e942b89a12fa9f8e0d
sbs0223 <100311853+sbs0223@users.noreply.github.com>
2022-09-03 00:34:52 -0400

    rebase

--- js/comments.js
+++ js/comments.js
@@ -192,7 +192,7 @@ function getcomments(){
 	}
   const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQABY2QntPAjViT8mZnc4X0Cx7KLnDg8j2k1sxKZNg9LuTHq26dAwY3gn8QSdwIGFa68rXPKAHo2zoS/pub?output=csv';
   alasql.promise("SELECT * FROM CSV(?, {headers:true}) where Series = '" + series + "' " + chapquery + "order by Replyto asc, Timestamp desc", [url]).then(function(results){
-		
+	  	$("#displaycomments").empty();
 		$("#displaycomments").append("<div class=\"commentwrapper\">");
 		
 		for(var i = 0; i < results.length; i++) {
